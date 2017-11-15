72223031a2361246ca48bc7cbba6249a8e2f4f1d
adtydhan <adtydhan@qq.com>
2017-11-15 08:55:16 +0800

    测

--- server/src/main/java/wujy/ServerApplication.java
+++ server/src/main/java/wujy/ServerApplication.java
@@ -1,4 +1,4 @@
-package main.java.wujy;
+package wujy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
